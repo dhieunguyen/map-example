62e90255f802c80975929ffdad0f7c258919cea3
hieund <ndhieuit209@gmail.com>
2022-03-08 22:36:30 +0700

    add search location

--- app/src/main/java/com/example/mapexample/MapsActivity.java
+++ app/src/main/java/com/example/mapexample/MapsActivity.java
@@ -2,7 +2,20 @@ package com.example.mapexample;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,24 +25,77 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.mapexample.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final String TAG = "MapActivity";
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-
+    private EditText mSearchText;
+    private Button mSearchBtn;
+    private RelativeLayout mRelativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        mSearchText = (EditText) findViewById(R.id.input_search);
+        mSearchBtn = (Button) findViewById(R.id.searchBtn);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.myLinearLayout);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    private void init(){
+        Log.d(TAG, "init: initializing");
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(mRelativeLayout.getWindowToken(), 0);
+                geoLocate();
+            }
+        });
+//        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+//                if(actionId == EditorInfo.IME_ACTION_SEARCH
+//                        || actionId == EditorInfo.IME_ACTION_DONE
+//                        || keyEvent.getAction() == KeyEvent.ACTION_DOWN
+//                        || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER){
+//                    geoLocate();
+//                }
+//                return false;
+//            }
+//        });
+    }
+    private void geoLocate(){
+        Log.d(TAG, "geoLocate: geolocating");
+        String searchString = mSearchText.getText().toString();
+        Geocoder geocoder = new Geocoder(MapsActivity.this);
+        List<Address> list = new ArrayList<>();
+        try{
+            list = geocoder.getFromLocationName(searchString, 1);
+        }catch (IOException e){
+            Log.e(TAG, "geoLocate: IOException: " + e.getMessage() );
+        }
+        if(list.size() > 0){
+            Address address = list.get(0);
 
+            Log.d(TAG, "geoLocate: found a location: " + address.toString());
+            this.moveCamera(new LatLng(address.getLatitude(),address.getLongitude()),18);
+            //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+
+        }
+    }
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -47,5 +113,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng lilama = new LatLng(20.9979105, 105.7914432);
         mMap.addMarker(new MarkerOptions().position(lilama).title("Marker in Lilama"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lilama,18));
+        init();
     }
 }
